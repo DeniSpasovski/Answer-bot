2e68e0682a0f1c0850f14a7e6dfb4218b86225ab
Deni <denimf@gmail.com>
2013-11-27 21:44:18 -0500

    basic-data.js fixed

--- basic-data.js
+++ basic-data.js
@@ -1,34 +1,35 @@
-var Keywords = [
-    { "keys": ["hi"], "value": 0 },
-    { "keys": ["hello"], "value": 0 },
-    { "keys": ["life", "universe", "everything"], "value": 1 },
-    { "keys": ["software", "development"], "value": 2 },
-    { "keys": ["software", "engineering"], "value": 2 },
-    { "keys": ["software", "sales"], "value": 3 },
-    { "keys": ["whatever", "sales"], "value": 3 }
-];
+if (answerBot) {
+    answerBot.prototype.specialContext = {
+        "wrongInput": ["I don't understand you.", "Could you rephrase the question?"],
+        "emptyInput": ["Please say something", "Speak louder", "Well i can't read minds."],
+        "rephrase": ["Can you tell me if your question was about one of the following things:"]
+    };
 
-var Content = [
-{
-    "description": "Did you mean hello?",
-    "values": ["I'm good how are you?", "Hi how can I help you today?"]
-},
-{
-    "description": "The answer to life the universe and everything?",
-    "values": ["42!"]
-},
-{
-    "description": "What is software development?",
-    "values": ["Programming! Do you speak it?"]
-},
-{
-    "description": "What is sales?",
-    "values": ["Cashing out!"]
+    answerBot.prototype.keywords = [
+        { "keys": ["hi"], "value": 0 },
+		{ "keys": ["hello"], "value": 0 },
+		{ "keys": ["life", "universe", "everything"], "value": 1 },
+		{ "keys": ["software", "development"], "value": 2 },
+		{ "keys": ["software", "engineering"], "value": 2 },
+		{ "keys": ["software", "sales"], "value": 3 },
+		{ "keys": ["whatever", "sales"], "value": 3 }];
+
+    answerBot.prototype.answers = [
+		{
+			"description": "Did you mean hello?",
+			"values": ["I'm good how are you?", "Hi how can I help you today?"]
+		},
+		{
+			"description": "The answer to life the universe and everything?",
+			"values": ["42!"]
+		},
+		{
+			"description": "What is software development?",
+			"values": ["Programming! Do you speak it?"]
+		},
+		{
+			"description": "What is sales?",
+			"values": ["Cashing out!"]
+		}
+	];
 }
-];
-
-SpecialContext = {
-    'wrongInput': ["I don't understand you.", "Could you rephrase the question?"],
-    'emptyInput': ["Please say something", "Speak louder", "Well i can't read minds."],
-    'rephrase': ["Can you tell me if your question was about one of the following things:"]
-};
